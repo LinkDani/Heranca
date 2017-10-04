9dc6d8e118bda2081dfdb4ba5b4a5224e6687db8
Daniel Walter <danielwalterpand@gmail.com>
2017-10-03 21:18:29 -0300

    Testando o Funcionário

---
+++ src/br/com/empresa/teste/TesteFuncionario.java
@@ -0,0 +1,17 @@
+package br.com.empresa.teste;
+
+import org.junit.Test;
+
+public class TesteFuncionario {
+	
+	protected String nome;
+	protected String cpf;
+	protected double salario;
+	
+	@Test
+	public void Funcionario() {
+		this.nome = "";
+		this.cpf = "";
+		this.salario = 0;
+	}
+}
--- src/br/com/empresa/teste/testeGerente.java
+++ src/br/com/empresa/teste/testeGerente.java
@@ -2,16 +2,17 @@ package br.com.empresa.teste;
 
 import org.junit.Test;
 
+import br.com.empresa.Funcionario;
 import br.com.empresa.Gerente;
 
 public class testeGerente {
 	@Test
-	public void TesteGerente(){
+	public void TesteGerente() {
 		Gerente gerente = new Gerente("Afonso", "29032001", 2000, 147258, 564);
 	}
-	
+
 	@Test
-	public void Autenticação(){
+	public void Autenticação() {
 		Gerente gerente = new Gerente("João", "", 2000, 444456, 4868);
 	}
 }
